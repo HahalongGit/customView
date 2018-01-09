7aa74b0f9f84ed3988fb1df0fb29a23004523c87
customView <729665268@qq.com>
2018-01-09 13:53:36 +0800

    模仿实现LoadingView

--- app/src/main/java/com/lll/beizertest/activity/StatusBarActivity.java
+++ app/src/main/java/com/lll/beizertest/activity/StatusBarActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -38,8 +39,9 @@ public class StatusBarActivity extends AppCompatActivity {
             getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
             getWindow().setStatusBarColor(Color.TRANSPARENT);
         }else {
-            // TODO: 2017/12/19 小于5.0 的需要添加一饿View设置
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         }
+        //状态栏设置，5.0以下的状态栏设置颜色要添加一个View在DecorView上操作
         toolBarStartBar = findViewById(R.id.toolBarStartBar);
         scrollView = findViewById(R.id.scrollView);
         tvTitile = findViewById(R.id.tv_titile);
