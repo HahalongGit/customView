9bfcb9a4fa7a801d2f09c3655e47f15facca6d53
customView <729665268@qq.com>
2018-03-13 15:20:54 +0800

    一个图片选择库的library，选择多张上传，不裁剪，没有压缩。用于易售后等项目。

---
+++ imageSelector/src/main/java/com/lll/imageselector/Test.java
@@ -0,0 +1,12 @@
+package com.lll.imageselector;
+
+/**
+ * Created by longlong on 2018/2/5.
+ *
+ * @ClassName: Test
+ * @Description:
+ * @Date 2018/2/5
+ */
+
+public class Test {
+}
---
+++ imageSelector/src/main/java/com/lll/imageselector/helpers/Constants.java
@@ -0,0 +1,44 @@
+package com.lll.imageselector.helpers;
+
+import android.os.Environment;
+
+import java.io.File;
+
+/**
+ * Created by Darshan on 5/26/2015.
+ */
+public class Constants {
+    public static final int PERMISSION_REQUEST_CODE = 1000;
+    public static final int PERMISSION_GRANTED = 1001;
+    public static final int PERMISSION_DENIED = 1002;
+
+    public static final int REQUEST_CODE = 2000;
+
+    public static final int FETCH_STARTED = 2001;
+    public static final int FETCH_COMPLETED = 2002;
+    public static final int ERROR = 2005;
+
+    /**
+     * Request code for permission has to be < (1 << 8)
+     * Otherwise throws java.lang.IllegalArgumentException: Can only use lower 8 bits for requestCode
+     */
+    public static final int PERMISSION_REQUEST_READ_EXTERNAL_STORAGE = 23;
+
+    public static final String INTENT_EXTRA_ALBUM = "album";
+    public static final String INTENT_EXTRA_IMAGES = "images";
+    public static final String INTENT_EXTRA_LIMIT = "limit";
+    public static final int DEFAULT_LIMIT = 10;
+
+    //Maximum number of images that can be selected at a time
+    public static int limit;
+
+    /**
+     * 拍照路径
+     */
+    public static File FILE_SAVE_PATH = new File(Environment.getExternalStoragePublicDirectory("kbb_finance"),"/camera");
+
+    /**
+     * 裁剪缓存
+     */
+    public static File FILE_SAVE_CACHE_PATH = new File(Environment.getExternalStoragePublicDirectory("kbb_finance"),"/camera/cache");
+}
