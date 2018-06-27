1f7c0ca83df348583f450606733c099547071656
customView <729665268@qq.com>
2018-06-27 10:37:55 +0800

    RxJava 分析

---
+++ app/src/main/java/com/lll/beizertest/activity/RxjavaActivity.java
@@ -0,0 +1,60 @@
+package com.lll.beizertest.activity;
+
+import android.graphics.Bitmap;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lll.beizertest.R;
+
+import java.util.Iterator;
+
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
+import io.reactivex.schedulers.Schedulers;
+
+/**
+ * RxJava2 分析和解析 示例
+ * RxJava 线程切换实现方式，AndroidSchedulers.mainThread() 实际上是创建了Handler处理消息切换，
+ * 调用 Schedulers.io()的时候创建一个Thread 处理异步的任务
+ * 实际上RxJava 中也是一种观察者模式的拓展使用。
+ */
+public class RxjavaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rxjava);
+        Observable.just("")
+                .map(new Function<String, Bitmap>() {
+                    @Override
+                    public Bitmap apply(String s) throws Exception {
+                        return null;
+                    }
+                }).subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnSubscribe(new Consumer<Disposable>() {
+                    @Override
+                    public void accept(Disposable disposable) throws Exception {
+                        //做一些准备工作，显示加载进度
+                    }
+                })
+                .subscribe(new Consumer<Bitmap>() {
+                    @Override
+                    public void accept(Bitmap bitmap) throws Exception {
+
+                    }
+                }, new Consumer<Throwable>() {
+                    @Override
+                    public void accept(Throwable throwable) throws Exception {
+
+                    }
+                });
+    }
+
+
+
+}
