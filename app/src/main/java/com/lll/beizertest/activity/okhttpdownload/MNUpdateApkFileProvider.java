b76e2af41b21b590cab8df36691cd5eac924cde5
liulonglong <longlong.liu@pcitc.com>
2019-10-14 10:36:07 +0800

    okhttp下载添加一个PesponseBody获取下载进度demo

---
+++ app/src/main/java/com/lll/beizertest/activity/okhttpdownload/MNUpdateApkFileProvider.java
@@ -0,0 +1,15 @@
+package com.lll.beizertest.activity.okhttpdownload;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ * <pre>
+ *     author : maning
+ *     e-mail : xxx@xx
+ *     time   : 2018/01/08
+ *     desc   : 更新专用FileProvider
+ *     version: 1.0
+ * </pre>
+ */
+public class MNUpdateApkFileProvider extends FileProvider {
+}
