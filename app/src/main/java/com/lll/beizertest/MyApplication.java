0234e552557aff0c9b38f59c130095280df8e110
LonglongLiu <longlong.liu@android.com>
2021-01-08 14:25:07 +0800

    添加bugly跟踪和method Tracing跟踪测试

--- app/src/main/java/com/lll/beizertest/MyApplication.java
+++ app/src/main/java/com/lll/beizertest/MyApplication.java
@@ -1,9 +1,11 @@
 package com.lll.beizertest;
 
 import android.app.Application;
+import android.os.Debug;
 
 import com.lll.beizertest.activity.ProxyActivity;
 import com.lll.beizertest.utils.HookStartActivityUtil;
+import com.tencent.bugly.crashreport.CrashReport;
 
 /**
  * Created by longlong on 2018/4/4.
@@ -18,6 +20,8 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Debug.startMethodTracing("longlongApp");
+        CrashReport.initCrashReport(getApplicationContext(), "e903b8d2b5", false);
         try {
             //对 插件化处理
             HookStartActivityUtil hookStartActivityUtil = new HookStartActivityUtil(this, ProxyActivity.class);
@@ -26,5 +30,6 @@ public class MyApplication extends Application {
         } catch (Exception e) {
             e.printStackTrace();
         }
+        Debug.stopMethodTracing();
     }
 }
