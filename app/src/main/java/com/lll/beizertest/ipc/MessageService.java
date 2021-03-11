8573296b03c3bb0ff4e71a23b057f2ba52660532
longlong <longlong@163.com>
2021-03-11 21:26:47 +0800

    注释掉aidl服务

--- app/src/main/java/com/lll/beizertest/ipc/MessageService.java
+++ app/src/main/java/com/lll/beizertest/ipc/MessageService.java
@@ -3,7 +3,6 @@ package com.lll.beizertest.ipc;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
-import android.os.RemoteException;
 import android.support.annotation.Nullable;
 
 /**
@@ -19,25 +18,26 @@ public class MessageService extends Service {
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        return userBinder;
+//        return userBinder;
+        return null;
     }
 
 
     /**
      * 实现一个Binder （Aidl 会自动生成一个Stub java文件 包含两个子类，Stub 和 Proxy）
      */
-    private UserAidl.Stub userBinder = new UserAidl.Stub() {
-        @Override
-        public String getUserName() throws RemoteException {
-
-            return "龙龙";
-        }
-
-        @Override
-        public String getUserPassword() throws RemoteException {
-
-            return "myPassword";
-        }
-
-    };
+//    private UserAidl.Stub userBinder = new UserAidl.Stub() {
+//        @Override
+//        public String getUserName() throws RemoteException {
+//
+//            return "龙龙";
+//        }
+//
+//        @Override
+//        public String getUserPassword() throws RemoteException {
+//
+//            return "myPassword";
+//        }
+//
+//    };
 }
