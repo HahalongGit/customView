64cddb8299c008b59dc3750321a7a585fd9ec737
liulonglong <liulonglong@zhihu.com>
2021-06-25 11:35:31 +0800

    添加了Team.java文件

---
+++ app/src/main/java/com/lll/beizertest/Team.java
@@ -0,0 +1,15 @@
+package com.lll.beizertest;
+
+/**
+ * TODO:describe what the class or interface does.
+ *
+ * @author liulonglong @Zhihu Inc.
+ * @date 6/25/21
+ */
+class Team {
+
+    private String teamId;
+
+    private String teamName;
+
+}
